b4f001a048ac45926188859d1d9084c713c306c6
nickc <nickc@10.1.81.174>
2017-09-22 10:16:17 -0700

    test commit 3

---
+++ edu.usfca.cs.cs245/ArrayQueue.java
@@ -0,0 +1,22 @@
+
+public class ArrayQueue implements Queue {
+
+	@Override
+	public Object dequeue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void enqueue(Object item) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean empty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ edu.usfca.cs.cs245/ArrayStack.java
@@ -0,0 +1,28 @@
+
+public class ArrayStack implements Stack {
+
+	@Override
+	public void push(Object item) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Object pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean empty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
